523699897da28c0ce07f29a73e4e7c38a8fc19a4
Kieron <kieronyolin12@gmail.com>
2025-06-15 02:32:46 +0700

    Functional Prototype Feed and Some Account Part

--- frontend/metro.config.js
+++ frontend/metro.config.js
@@ -1,6 +1,15 @@
 const { getDefaultConfig } = require("expo/metro-config");
 const { withNativeWind } = require('nativewind/metro');
- 
-const config = getDefaultConfig(__dirname)
- 
-module.exports = withNativeWind(config, { input: './global.css' })
+
+// get default Expo config
+const config = getDefaultConfig(__dirname);
+
+// remove 'svg' from assetExts and add to sourceExts
+config.resolver.assetExts = config.resolver.assetExts.filter(ext => ext !== 'svg');
+config.resolver.sourceExts = [...config.resolver.sourceExts, 'svg'];
+
+// attach svg-transformer into NativeWind
+config.transformer.babelTransformerPath = require.resolve("react-native-svg-transformer");
+
+// finally wrap with NativeWind
+module.exports = withNativeWind(config, { input: './global.css' });
