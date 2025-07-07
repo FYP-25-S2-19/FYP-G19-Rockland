95edb0488c597d35bb198d79c12cfadb3a8d78bb
thebigboss-gif <kennethjosephr7@gmail.com>
2025-07-07 22:08:15 +0800

    registration page is fixed + interest is now from database

--- frontend-web/.next/static/chunks/webpack.js
+++ frontend-web/.next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "aee79b2f0d467d6a"; }
+/******/ 		__webpack_require__.h = function() { return "0841a0f3d58976e3"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
