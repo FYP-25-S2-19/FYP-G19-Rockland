cb74bac541f68a66a81bf93a3ca0751fcb2f88f2
thebigboss-gif <kennethjosephr7@gmail.com>
2025-07-08 01:35:14 +0800

    fixed some issues on delete articles

--- frontend-web/.next/static/chunks/webpack.js
+++ frontend-web/.next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "044458645bd885b9"; }
+/******/ 		__webpack_require__.h = function() { return "4a048cd75093255b"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
