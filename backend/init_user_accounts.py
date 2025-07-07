3d03f71767530a56da8f9c3d3c5a48925b1a745f
thebigboss-gif <kennethjosephr7@gmail.com>
2025-07-08 01:04:10 +0800

    articles + application expert done

--- backend/init_user_accounts.py
+++ backend/init_user_accounts.py
@@ -13,6 +13,8 @@ from app.entity.token import Token
 from app.entity.application import Application
 from app.entity.application_answer import ApplicationAnswer
 from app.entity.application_file import ApplicationFile
+from app.entity.article import Article
+from app.entity.article_like import ArticleLike
 from datetime import datetime
 from sqlalchemy import inspect
 
